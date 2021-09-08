450d9ace9a79af086e0f4aaa45be8f8227c72c3f
alvesnet-suporte <86562232+alvesnet-suporte@users.noreply.github.com>
2021-09-08 16:04:10 -0600

    Windows Update Desativar
    Windows Update Desativar

---
+++ desativar_windows_update.bat
@@ -0,0 +1,11 @@
+c:\windows\system32\sc.exe config wuauserv start=disabled && c:\windows\system32\mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'Windows Update: DESATIVADO!', 10, 'Windows Update!', 48 );close()"
+
+
+
+
+
+
+
+
+
+
