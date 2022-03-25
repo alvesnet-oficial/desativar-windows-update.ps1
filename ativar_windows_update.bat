87451904d1843b130522c22cc06f686060afdf7b
alvesnet-suporte <86562232+alvesnet-suporte@users.noreply.github.com>
2022-03-25 18:18:41 -0300

    Atualizado em 25 de Março de 2022

---
+++ ativar_windows_update.bat
@@ -0,0 +1,12 @@
+c:\windows\system32\sc.exe config wuauserv start=auto && c:\windows\system32\mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'Windows Update: ATIVADO!', 10, 'Windows Update!', 48 );close()"
+c:\windows\system32\sc.exe start wuauserv
+
+
+
+
+
+
+
+
+
+
--- desativar_windows_update.bat
+++ desativar_windows_update.bat
@@ -1,4 +1,5 @@
 c:\windows\system32\sc.exe config wuauserv start=disabled && c:\windows\system32\mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'Windows Update: DESATIVADO!', 10, 'Windows Update!', 48 );close()"
+c:\windows\system32\sc.exe stop wuauserv
 
 
 
